ba6b7fe91f0d1f6a2d2951ed4ed8890aa1fe66dc
Louis <me@louiscb.com>
2021-11-17 13:56:57 +0100

    Add exercise 3 and rename files from ex

---
+++ Assignment_2/ex_3/exercise3.cu
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+#define BLOCK_SIZE 256
+#define ERROR_RANGE 1E-9
+
+typedef struct {
+    float3 pos;
+    float3 vel;
+} Particle;
+
+__global__ void oneTimestepGPU(Particle *p) {
+    const int id = blockIdx.x * blockDim.x + threadIdx.x;
+    p[id].pos.x += p[id].vel.x;
+    p[id].pos.y += p[id].vel.y;
+    p[id].pos.z += p[id].vel.z;
+}
+
+void oneTimestepCPU(Particle *p, int n) {
+    for (int i = 0; i < n; i++) {
+        p[i].pos.x += p[i].vel.x;
+        p[i].pos.y += p[i].vel.y;
+        p[i].pos.z += p[i].vel.z;
+    }
+}
+
+double cpuSecond() {
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    return ((double) tp.tv_sec + (double) tp.tv_usec * 1.e-6);
+}
+
+int main(int argc, char *argv[]) {
+    int NUM_PARTICLES = atoi(argv[1]);
+    int NUM_ITERATIONS = atoi(argv[2]);
+    Particle pCPU[NUM_PARTICLES];
+    Particle pGPUres[NUM_PARTICLES];
+    Particle *pGPU;
+    int nBlocks;
+    double iStart, iElapsCPU, iElapsGPU;
+    double error;
+    int nErrors;
+    nBlocks = (NUM_PARTICLES + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    srand(time(0));
+
+    for (int i = 0; i < NUM_PARTICLES; i++) {
+        pCPU[i].pos.x = rand();
+        pCPU[i].pos.y = rand();
+        pCPU[i].pos.z = rand();
+        pCPU[i].vel.x = rand();
+        pCPU[i].vel.y = rand();
+        pCPU[i].vel.z = rand();
+    }
+
+    cudaMalloc(&pGPU, NUM_PARTICLES * sizeof(Particle));
+    iStart = cpuSecond();
+
+    for (int i = 0; i < NUM_ITERATIONS; i++) {
+        oneTimestepCPU(pCPU, NUM_PARTICLES);
+    }
+
+    iElapsCPU = cpuSecond() - iStart;
+    iStart = cpuSecond();
+    cudaMemcpy(pGPU, pCPU, NUM_PARTICLES * sizeof(Particle), cudaMemcpyHostToDevice);
+
+    for (int i = 0; i < NUM_ITERATIONS; i++) {
+        oneTimestepGPU<<<nBlocks, BLOCK_SIZE>>>(pGPU);
+        cudaDeviceSynchronize();
+    }
+
+    cudaMemcpy(pGPUres, pGPU, NUM_PARTICLES * sizeof(Particle), cudaMemcpyDeviceToHost);
+    iElapsGPU = cpuSecond() - iStart;
+    cudaFree(pGPU);
+    nErrors = 0;
+
+    for (int i = 0; i < NUM_PARTICLES; i++) {
+        error = fabs(pCPU[i].pos.x - pGPUres[i].pos.x);
+
+        if (error > ERROR_RANGE) {
+            nErrors++;
+            continue;
+        }
+
+        error = fabs(pCPU[i].pos.y - pGPUres[i].pos.y);
+
+        if (error > ERROR_RANGE) {
+            nErrors++;
+            continue;
+        }
+
+        error = fabs(pCPU[i].pos.z - pGPUres[i].pos.z);
+
+        if (error > ERROR_RANGE) {
+            nErrors++;
+            continue;
+        }
+    }
+
+    printf("%d %d %f %f %d\n", NUM_PARTICLES, NUM_ITERATIONS, iElapsCPU, iElapsGPU, nErrors);
+}
---
+++ Assignment_2/ex_3/loop.sh
@@ -0,0 +1,4 @@
+for number in {1..10}
+do
+  ./ex3.out $((1000 * number)) $((10 * number))
+done
