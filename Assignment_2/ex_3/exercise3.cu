363006b2561278ce5d982e5570034a46eacbfb01
Joakim Eriksson <joakieri.kth@gmail.com>
2021-11-18 14:49:03 +0100

    Extended the test script

--- Assignment_2/ex_3/exercise3.cu
+++ Assignment_2/ex_3/exercise3.cu
@@ -1,28 +1,25 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-#define BLOCK_SIZE 256
-#define ERROR_RANGE 1E-9
+#define ERROR_RANGE 1e-9
 
 typedef struct {
     float3 pos;
     float3 vel;
 } Particle;
 
-void oneTimeStep(Particle *p) {
-    p.pos.x += p.vel.x;
-    p.pos.y += p.vel.y;
-    p.pos.z += p.vel.z;
-}
-
 __global__ void oneTimestepGPU(Particle *p) {
-    const int id = blockIdx.x * blockDim.x + threadIdx.x;
-    oneTimeStep(p[id]);
+    const int id = blockIdx.x * blockDim.x + threadIdx.x;  
+    p[id].pos.x += p[id].vel.x;
+    p[id].pos.y += p[id].vel.y;
+    p[id].pos.z += p[id].vel.z;
 }
 
 void oneTimestepCPU(Particle *p, int n) {
     for (int i = 0; i < n; i++) {
-        oneTimeStep(p[i]);
+	    p[i].pos.x += p[i].vel.x;
+	    p[i].pos.y += p[i].vel.y;
+	    p[i].pos.z += p[i].vel.z;
     }
 }
 
@@ -35,16 +32,21 @@ double cpuSecond() {
 int main(int argc, char *argv[]) {
     int NUM_PARTICLES = atoi(argv[1]);
     int NUM_ITERATIONS = atoi(argv[2]);
+    int BLOCK_SIZE = atoi(argv[3]);
     Particle pCPU[NUM_PARTICLES];
+    Particle pCPUres[NUM_PARTICLES];
     Particle pGPUres[NUM_PARTICLES];
     Particle *pGPU;
     int nBlocks;
     double iStart, iElapsCPU, iElapsGPU;
     double error;
     int nErrors;
+
+
+    //printf("Particles, Iterations, Thread block size, CPU time, GPU time, Errors\n");
+
     nBlocks = (NUM_PARTICLES + BLOCK_SIZE - 1) / BLOCK_SIZE;
     srand(time(0));
-
     for (int i = 0; i < NUM_PARTICLES; i++) {
         pCPU[i].pos.x = rand();
         pCPU[i].pos.y = rand();
@@ -52,51 +54,50 @@ int main(int argc, char *argv[]) {
         pCPU[i].vel.x = rand();
         pCPU[i].vel.y = rand();
         pCPU[i].vel.z = rand();
+	pCPUres[i] = pCPU[i];
     }
-
+    
     cudaMalloc(&pGPU, NUM_PARTICLES * sizeof(Particle));
+
+    // Meassure CPU performance
     iStart = cpuSecond();
-
     for (int i = 0; i < NUM_ITERATIONS; i++) {
-        oneTimestepCPU(pCPU, NUM_PARTICLES);
+        oneTimestepCPU(pCPUres, NUM_PARTICLES);
     }
-
     iElapsCPU = cpuSecond() - iStart;
+    
+    // Meassure GPU performance
     iStart = cpuSecond();
     cudaMemcpy(pGPU, pCPU, NUM_PARTICLES * sizeof(Particle), cudaMemcpyHostToDevice);
-
     for (int i = 0; i < NUM_ITERATIONS; i++) {
         oneTimestepGPU<<<nBlocks, BLOCK_SIZE>>>(pGPU);
         cudaDeviceSynchronize();
     }
-
     cudaMemcpy(pGPUres, pGPU, NUM_PARTICLES * sizeof(Particle), cudaMemcpyDeviceToHost);
     iElapsGPU = cpuSecond() - iStart;
     cudaFree(pGPU);
+
+    // Check the number of errors
     nErrors = 0;
-
     for (int i = 0; i < NUM_PARTICLES; i++) {
-        error = fabs(pCPU[i].pos.x - pGPUres[i].pos.x);
-
+        error = fabs(pCPUres[i].pos.x - pGPUres[i].pos.x);
         if (error > ERROR_RANGE) {
             nErrors++;
             continue;
         }
 
-        error = fabs(pCPU[i].pos.y - pGPUres[i].pos.y);
-
+        error = fabs(pCPUres[i].pos.y - pGPUres[i].pos.y);
         if (error > ERROR_RANGE) {
             nErrors++;
             continue;
         }
 
-        error = fabs(pCPU[i].pos.z - pGPUres[i].pos.z);
-
+        error = fabs(pCPUres[i].pos.z - pGPUres[i].pos.z);
         if (error > ERROR_RANGE) {
             nErrors++;
             continue;
         }
     }
 
-    printf("%d %d %f %f %d\n", NUM_PARTICLES, NUM_ITERATIONS, iElapsCPU, iElapsGPU, nErrors);
+    printf("%d %d %d %f %f %d\n", NUM_PARTICLES, NUM_ITERATIONS, BLOCK_SIZE, iElapsCPU, iElapsGPU, nErrors);
 }
--- Assignment_2/ex_3/loop.sh
+++ Assignment_2/ex_3/loop.sh
@@ -1,4 +1,22 @@
-for number in {1..10}
+echo "Particles, Iterations, Thread block size, CPU time, GPU time, Errors"
+
+echo ""
+echo "--- Part 1, testing CPU ---"
+echo ""
+
+for particles in {10,100,1000,10000,100000}
 do
-  ./ex3.out $((1000 * number)) $((10 * number))
+	./ex3.out $((particles)) $((500)) $((256))
+done
+
+echo ""
+echo "--- Part 2, testing GPU ---"
+echo ""
+
+for number in {1,2,4,8,16}
+do
+	for particles in {10,100,1000,10000,100000}
+	do
+		./ex3.out $((particles)) $((500)) $((16 * number))
+	done
 done
